f5f6de82ac1e130974b1c373dacdbdb9e79ba467
ajbenn90 <89327140+ajbenn90@users.noreply.github.com>
2022-04-23 09:53:41 -0600

    Updated documentation
    Added some hints

--- src/dynamicList/IntList.java
+++ src/dynamicList/IntList.java
@@ -1,7 +1,47 @@
 package dynamicList;
 
 /**
- * Defines the methods that a dynamic list of integers must implement.
+ * <p>
+ * This interface defines the methods that a dynamic list of integers must
+ * implement.
+ * </p>
+ * <p>
+ * I'd recommend implementing <i>insert</i> or <i>append</i> first.
+ * <i>append</i> is easier to implement, but if you're clever, you can use
+ * <i>insert</i> inside of other methods to avoid code repitition.
+ * </p>
+ * <p>
+ * Side note: While you may have to increase the size of the backing array after
+ * an insert, you do not have to decrease the size of the backing array after a
+ * removal. You can if you want to, but it's not required. If you choose to do
+ * so, don't decrease the size of the array every time an element is removed.
+ * (That would be expensive). Think about a threshold would be provide a good
+ * compromise between preserving memory and keeping it inexpensive.
+ * </p>
+ * <p>
+ * For a small hint, here's how many lines it took me to implement each method
+ * (not including brackets). Note that I also created a helper method for
+ * changing the size of the backing array, which I don't include in the
+ * calculations below.
+ * <ul>
+ * <li>append - 1</li>
+ * <li>appendAll - 2</li>
+ * <li>clear - 1</li>
+ * <li>contains - 1</li>
+ * <li>equals - 8</li>
+ * <li>indexOf - 4</li>
+ * <li>insert - 8</li>
+ * <li>isEmpty - 1</li>
+ * <li>lastIndexOf - 4</li>
+ * <li>prepend - 1</li>
+ * <li>remove - 7</li>
+ * <li>set - 5</li>
+ * <li>size - 1</li>
+ * <li>toArray - 4</li>
+ * <li>trimToSize - 1</li>
+ * </ul>
+ * 
+ * </p>
  * 
  * @author alex
  *
@@ -37,8 +77,8 @@ public interface IntList {
 
 	/**
 	 * Checks if the given list is the same as this list, i.e. has the same values
-	 * in the same order. If <i>other</i> is null, return false instead of throwing an
-	 * exception.
+	 * in the same order. If <i>other</i> is null, return false instead of throwing
+	 * an exception.
 	 * 
 	 * @param other
 	 * @return
