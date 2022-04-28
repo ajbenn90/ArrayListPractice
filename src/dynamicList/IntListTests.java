ff64e9964aabf37d8f9aedbbd8109f99622e28a0
ajbenn90 <89327140+ajbenn90@users.noreply.github.com>
2022-04-28 00:23:40 -0600

    Added tests

--- src/dynamicList/IntListTests.java
+++ src/dynamicList/IntListTests.java
@@ -37,6 +37,20 @@ class IntListTests {
 	}
 
 	/**
+	 * Append 1000 elements. This should guarantee a resize.
+	 */
+	@Test
+	void appendTest03() {
+		IntDynamicList l = new IntDynamicList();
+		for (int i = 0; i < 1000; i++)
+			l.append(i);
+
+		assertEquals(1000, l.size());
+		for (int i = 0; i < 1000; i++)
+			assertEquals(i, l.get(i));
+	}
+
+	/**
 	 * Tests appending a list to an empty list.
 	 */
 	@Test
@@ -67,6 +81,52 @@ class IntListTests {
 		assertEquals(4, l.size());
 		for (int i = 0; i < l.size(); i++)
 			assertEquals(i, l.get(i));
+	}
+
+	/**
+	 * Append 1000 elements. This should guarantee a resize.
+	 */
+	@Test
+	void appendAllTest03() {
+		IntDynamicList l1 = new IntDynamicList();
+		for (int i = 0; i < 1000; i++)
+			l1.append(i);
+
+		IntDynamicList l2 = new IntDynamicList();
+		l2.appendAll(l1);
+
+		assertEquals(1000, l2.size());
+		for (int i = 0; i < 1000; i++)
+			assertEquals(i, l2.get(i));
+	}
+
+	/**
+	 * Append an empty to list to this list
+	 */
+	@Test
+	void appendAllTest04() {
+		IntDynamicList l1 = new IntDynamicList();
+		l1.append(0);
+
+		IntDynamicList l2 = new IntDynamicList();
+		l1.appendAll(l2);
+
+		int[] expected = { 0 };
+		assertArrayEquals(expected, l1.toArray());
+	}
+
+	/**
+	 * Append an empty to list to an empty list
+	 */
+	@Test
+	void appendAllTest05() {
+		IntDynamicList l1 = new IntDynamicList();
+
+		IntDynamicList l2 = new IntDynamicList();
+		l1.appendAll(l2);
+
+		int[] expected = {};
+		assertArrayEquals(expected, l1.toArray());
 	}
 
 	/**
@@ -643,6 +703,54 @@ class IntListTests {
 	}
 
 	/**
+	 * Insert 1000 elements at the end. Guarantees a resize.
+	 */
+	@Test
+	void insertTest09() {
+		IntDynamicList l = new IntDynamicList();
+		int[] expected = new int[1000];
+		for (int i = 0; i < 1000; i++) {
+			l.insert(i, i);
+			expected[i] = i;
+		}
+
+		assertArrayEquals(expected, l.toArray());
+	}
+
+	/**
+	 * Insert 1000 elements at the beginning. Guarantees a resize.
+	 */
+	@Test
+	void insertTest10() {
+		IntDynamicList l = new IntDynamicList();
+		int[] expected = new int[1000];
+		for (int i = 0; i < 1000; i++) {
+			l.insert(0, i);
+			expected[999 - i] = i;
+		}
+
+		assertArrayEquals(expected, l.toArray());
+	}
+
+	/**
+	 * Insert 1000 elements at index 1 (after one element inserted). Guarantees a
+	 * resize.
+	 */
+	@Test
+	void insertTest11() {
+		IntDynamicList l = new IntDynamicList();
+		int[] expected = new int[1000];
+		l.append(999);
+		expected[0] = 999;
+		for (int i = 0; i < 999; i++) {
+			l.insert(1, i);
+			expected[999 - i] = i;
+		}
+
+		assertArrayEquals(expected, l.toArray());
+	}
+
+	/**
 	 * Empty list
 	 */
 	@Test
@@ -679,7 +787,7 @@ class IntListTests {
 	 * If the list is empty, should return -1
 	 */
 	@Test
-	void lastlastIndexOfTest01() {
+	void lastIndexOfTest01() {
 		IntDynamicList l = new IntDynamicList();
 
 		assertEquals(-1, l.lastIndexOf(0));
@@ -691,7 +799,7 @@ class IntListTests {
 	 * If a list of length 1 does not contain the element, should return -1
 	 */
 	@Test
-	void lastlastIndexOfTest02() {
+	void lastIndexOfTest02() {
 		IntDynamicList l = new IntDynamicList();
 		l.append(0);
 
@@ -702,7 +810,7 @@ class IntListTests {
 	 * If a list does not contain the element, should return -1
 	 */
 	@Test
-	void lastlastIndexOfTest03() {
+	void lastIndexOfTest03() {
 		IntDynamicList l = new IntDynamicList();
 		l.append(4);
 		l.append(5);
@@ -715,7 +823,7 @@ class IntListTests {
 	 * Test on a length 1 list with an instance of the element being searched for
 	 */
 	@Test
-	void lastlastIndexOfTest04() {
+	void lastIndexOfTest04() {
 		IntDynamicList l = new IntDynamicList();
 		l.append(4);
 
@@ -726,7 +834,7 @@ class IntListTests {
 	 * Test on a list that contains one of the given element
 	 */
 	@Test
-	void lastlastIndexOfTest05() {
+	void lastIndexOfTest05() {
 		IntDynamicList l = new IntDynamicList();
 		l.append(1);
 		l.append(4);
@@ -739,7 +847,7 @@ class IntListTests {
 	 * Test on a list that contains several of the given element
 	 */
 	@Test
-	void lastlastIndexOfTest06() {
+	void lastIndexOfTest06() {
 		IntDynamicList l = new IntDynamicList();
 		l.append(1);
 		l.append(4);
@@ -755,7 +863,7 @@ class IntListTests {
 	 * Test on a list that contains the given element only at the beginning
 	 */
 	@Test
-	void lastlastIndexOfTest07() {
+	void lastIndexOfTest07() {
 		IntDynamicList l = new IntDynamicList();
 		l.append(1);
 		l.append(4);
@@ -771,7 +879,7 @@ class IntListTests {
 	 * Test on a list that contains the given element at the end
 	 */
 	@Test
-	void lastlastIndexOfTest08() {
+	void lastIndexOfTest08() {
 		IntDynamicList l = new IntDynamicList();
 		l.append(4);
 		l.append(4);
@@ -781,6 +889,313 @@ class IntListTests {
 		l.append(4);
 
 		assertEquals(5, l.lastIndexOf(4));
+	}
+
+	/**
+	 * Prepend a value in an empty list.
+	 */
+	@Test
+	void prependTest01() {
+		IntDynamicList l = new IntDynamicList();
+		l.prepend(1);
+
+		assertEquals(1, l.size());
+		assertEquals(1, l.get(0));
+	}
+
+	/**
+	 * Prepend a value in to a list of length 1.
+	 */
+	@Test
+	void prependTest02() {
+		IntDynamicList l = new IntDynamicList();
+		l.prepend(1);
+		l.prepend(2);
+
+		assertEquals(2, l.size());
+		assertEquals(2, l.get(0));
+		assertEquals(1, l.get(1));
+	}
+
+	/**
+	 * Prepend a value in to a list of length 2.
+	 */
+	@Test
+	void prependTest03() {
+		IntDynamicList l = new IntDynamicList();
+		l.prepend(1);
+		l.prepend(2);
+		l.prepend(3);
+
+		assertEquals(3, l.size());
+		assertEquals(3, l.get(0));
+		assertEquals(2, l.get(1));
+		assertEquals(1, l.get(2));
+	}
+	
+	/**
+	 * Prepend 1000 values. Guarantees a resize.
+	 */
+	@Test
+	void prependTest04() {
+		IntDynamicList l = new IntDynamicList();
+		int[] expected = new int[1000];
+		for (int i = 0; i < 1000; i++) {
+			l.prepend(i);
+			expected[999 - i] = i;
+		}
+		
+		assertArrayEquals(expected, l.toArray());
+	}
+
+	/**
+	 * Remove from a list of length 1.
+	 */
+	@Test
+	void removeTest01() {
+		IntDynamicList l = new IntDynamicList();
+		l.append(1);
+		l.remove(0);
+
+		assertEquals(0, l.size());
+	}
+
+	/**
+	 * Remove from the beginning of a list of length 2.
+	 */
+	@Test
+	void removeTest02() {
+		IntDynamicList l = new IntDynamicList();
+		l.append(1);
+		l.append(2);
+		l.remove(0);
+
+		assertEquals(1, l.size());
+		assertEquals(2, l.get(0));
+	}
+
+	/**
+	 * Remove from the end of a list of length 2.
+	 */
+	@Test
+	void removeTest03() {
+		IntDynamicList l = new IntDynamicList();
+		l.append(0);
+		l.append(1);
+		l.remove(1);
+
+		assertEquals(1, l.size());
+		assertEquals(0, l.get(0));
+	}
+
+	/**
+	 * Throws an exception when removing from an empty list. And size remains 0.
+	 */
+	@Test
+	void removeTest04() {
+		IntDynamicList l = new IntDynamicList();
+
+		assertThrows(IndexOutOfBoundsException.class, () -> {
+			l.remove(0);
+		});
+		assertEquals(0, l.size());
+	}
+
+	/**
+	 * Throws an exception when removing from an invalid index in a non-empty list.
+	 */
+	@Test
+	void removeTest05() {
+		IntDynamicList l = new IntDynamicList();
+		l.append(0);
+
+		assertThrows(IndexOutOfBoundsException.class, () -> {
+			l.remove(1);
+		});
+		assertThrows(IndexOutOfBoundsException.class, () -> {
+			l.remove(-1);
+		});
+	}
+
+	/**
+	 * Remove from the middle of a list of length 3.
+	 */
+	@Test
+	void removeTest06() {
+		IntDynamicList l = new IntDynamicList();
+		l.append(0);
+		l.append(1);
+		l.append(2);
+		l.remove(1);
+
+		assertEquals(2, l.size());
+		assertEquals(0, l.get(0));
+		assertEquals(2, l.get(1));
+	}
+
+	/**
+	 * Setting at an invalid index throws an exception.
+	 */
+	@Test
+	void setTest01() {
+		IntDynamicList l = new IntDynamicList();
+
+		assertThrows(IndexOutOfBoundsException.class, () -> {
+			l.set(0, 0);
+		});
+		assertThrows(IndexOutOfBoundsException.class, () -> {
+			l.set(-1, 0);
+		});
+		assertThrows(IndexOutOfBoundsException.class, () -> {
+			l.set(1, 0);
+		});
+	}
+
+	/**
+	 * Set index 0 of a one element list.
+	 */
+	@Test
+	void setTest02() {
+		IntDynamicList l = new IntDynamicList();
+		l.append(0);
+		l.set(0, 1);
+
+		assertEquals(1, l.get(0));
+		assertEquals(1, l.size());
+	}
+
+	/**
+	 * Set index several indexes of a large list.
+	 */
+	@Test
+	void setTest03() {
+		IntDynamicList l = new IntDynamicList();
+		l.append(0);
+		l.append(1);
+		l.append(2);
+		l.append(3);
+		l.append(4);
+		l.append(5);
+		l.set(0, 6);
+		l.set(1, 7);
+		l.set(2, 8);
+		l.set(3, 9);
+		l.set(4, 10);
+		l.set(5, 11);
+
+		int[] expected = { 6, 7, 8, 9, 10, 11 };
+		assertArrayEquals(expected, l.toArray());
+	}
+
+	/**
+	 * Size of empty list is 0
+	 */
+	@Test
+	void sizeTest01() {
+		IntDynamicList l = new IntDynamicList();
+
+		assertEquals(0, l.size());
+	}
+
+	/**
+	 * Length 1 list
+	 */
+	@Test
+	void sizeTest02() {
+		IntDynamicList l = new IntDynamicList();
+		l.append(0);
+
+		assertEquals(1, l.size());
+	}
+
+	/**
+	 * Length 0
+	 */
+	@Test
+	void toArrayTest01() {
+		IntDynamicList l = new IntDynamicList();
+
+		int[] expected = {};
+		assertArrayEquals(expected, l.toArray());
+	}
+
+	/**
+	 * Length 1
+	 */
+	@Test
+	void toArrayTest02() {
+		IntDynamicList l = new IntDynamicList();
+		l.append(1);
+
+		int[] expected = { 1 };
+		assertArrayEquals(expected, l.toArray());
+	}
+
+	/**
+	 * Length 5
+	 */
+	@Test
+	void toArrayTest03() {
+		IntDynamicList l = new IntDynamicList();
+		l.append(1);
+		l.append(3);
+		l.append(5);
+		l.append(7);
+		l.append(9);
+
+		int[] expected = { 1, 3, 5, 7, 9 };
+		assertArrayEquals(expected, l.toArray());
+	}
+
+	/**
+	 * Empty list. No exceptions thrown.
+	 */
+	@Test
+	void trimToSizeTest01() {
+		IntDynamicList l = new IntDynamicList();
+		l.trimToSize();
+
+		assertEquals(0, l.size());
+	}
+
+	/**
+	 * Append after empty trimToSize. No exceptions.
+	 */
+	@Test
+	void trimToSizeTest02() {
+		IntDynamicList l = new IntDynamicList();
+		l.trimToSize();
+		l.append(0);
+
+		int[] expected = { 0 };
+		assertArrayEquals(expected, l.toArray());
+	}
+
+	/**
+	 * Length 1 trim. Nothing changed.
+	 */
+	@Test
+	void trimToSizeTest03() {
+		IntDynamicList l = new IntDynamicList();
+		l.append(0);
+		l.trimToSize();
+
+		int[] expected = { 0 };
+		assertArrayEquals(expected, l.toArray());
+	}
+
+	/**
+	 * Length 5 trim. Nothing changed.
+	 */
+	@Test
+	void trimToSizeTest04() {
+		IntDynamicList l = new IntDynamicList();
+		for (int i = 0; i < 5; i++)
+			l.append(i);
+		l.trimToSize();
+
+		int[] expected = { 0, 1, 2, 3, 4 };
+		assertArrayEquals(expected, l.toArray());
 	}
 
 }
